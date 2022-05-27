78ae51df61598f1acd2dca88aa8d70a247e7f9c1
sathush <sathush3@gmail.com>
2022-05-27 09:36:07 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Sathurshan Ramachandran on 2022-05-27.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
